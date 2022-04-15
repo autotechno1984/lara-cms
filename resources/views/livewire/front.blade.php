e807680d321cdee53e994b59e63e8269f37208f6
budimax88 <84623214+daveveron84@users.noreply.github.com>
2022-04-15 21:42:34 +0700

    change progress bar

--- resources/views/livewire/front.blade.php
+++ resources/views/livewire/front.blade.php
@@ -43,10 +43,26 @@
                         </div>
                     </div>
                     <div>
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-striped progress-bar-animated py-1" style="width:{{ $data->winrate.'%' }};">
-                                {{$data->winrate.'%'}}</div>
+                        <div>
+                            @if($data->winrate < 30)
+                                <div class="progress" style="height: 25px; margin-top:5px;">
+                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style=" width: {{$data->winrate.'%' }}">{{ $data->winrate.'%' }}</div>
+                                </div>
+                            @elseif($data->winrate < 65)
+                                <div class="progress" style="height: 25px; margin-top:5px;">
+                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style=" width: {{$data->winrate.'%' }}">{{ $data->winrate.'%' }}</div>
+                                </div>
+                            @elseif($data->winrate < 80)
+                                <div class="progress" style="height: 25px; margin-top:5px;">
+                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style=" width: {{$data->winrate.'%' }}">{{ $data->winrate.'%' }}</div>
+                                </div>
+                            @else
+                                <div class="progress" style="height: 25px; margin-top:5px;">
+                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style=" width: {{$data->winrate.'%' }}">{{ $data->winrate.'%' }}</div>
+                                </div>
+                            @endif
                         </div>
+
                     </div>
                     <div style="padding-left:5px; background:red; color:white;" class="">
                         <div class="row">
